40edba0093bab4639261a79a2a0cb479ebda64e9
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-11-04 16:46:07 +0200

    add register

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 
+import authRouter from "./routes/auth.js";
 import moviesRouter from "./routes/movies.js";
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", authRouter);
 app.use("/api/movies", moviesRouter);
 
 app.use((_, res) => {
---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { nanoid } from "nanoid";
+
+import { User } from "../models/users.js";
+import { ctrlWrapper } from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
+import { sendEmail } from "../helpers/sendEmail.js";
+
+const { SECRET_KEY, PROJECT_URL } = process.env;
+
+const register = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already exist");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const verificationCode = nanoid();
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    verificationCode,
+  });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${PROJECT_URL}/api/auth/verify/${verificationCode}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  const { _id: id } = newUser;
+
+  const payload = {
+    id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.status(201).json({
+    token,
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+    },
+  });
+};
+
+export default {
+  register: ctrlWrapper(register),
+};
